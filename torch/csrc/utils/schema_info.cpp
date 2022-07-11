15841de33ca1fb087c78c99e1a74b6671490f2a8
goldenxuett <ttanx@fb.com>
2022-07-11 15:01:51 -0700

    [JIT] Add aliasing checks in SchemaInfo with associated tests
    ghstack-source-id: 1971219f6e3c50dd90f05bb97477e84fa5dda501
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/80984

--- torch/csrc/utils/schema_info.cpp
+++ torch/csrc/utils/schema_info.cpp
@@ -58,10 +58,43 @@ bool SchemaInfo::is_mutable(c10::string_view name) {
   return is_mutable(*index);
 }
 
+bool SchemaInfo::may_alias(
+    const c10::SchemaArgument& lhs,
+    const c10::SchemaArgument& rhs) {
+  bool basic_check = schema_.may_alias(lhs, rhs);
+  if (basic_check) {
+    return true;
+  }
+  if (!updated_) {
+    generateAliasMaps();
+  }
+  if (lhs.type == c10::SchemaArgType::input &&
+      rhs.type == c10::SchemaArgType::input) {
+    return input_alias_map_[lhs.index].count(rhs.index);
+  } else if (
+      lhs.type == c10::SchemaArgType::output &&
+      rhs.type == c10::SchemaArgType::output) {
+    for (size_t lhs_alias_input : output_alias_map_[lhs.index]) {
+      for (size_t rhs_alias_input : output_alias_map_[rhs.index]) {
+        if (lhs_alias_input == rhs_alias_input) {
+          return true;
+        }
+      }
+    }
+    return false;
+  } else if (lhs.type == c10::SchemaArgType::output) {
+    return output_alias_map_[lhs.index].count(rhs.index);
+  } else {
+    return output_alias_map_[rhs.index].count(lhs.index);
+  }
+}
+
 void SchemaInfo::generateAliasMaps() {
   updated_ = true;
   input_alias_map_ = std::vector<std::unordered_set<size_t>>(
       schema_.arguments().size(), std::unordered_set<size_t>());
+  output_alias_map_ = std::vector<std::unordered_set<size_t>>(
+      schema_.returns().size(), std::unordered_set<size_t>());
   for (size_t i = 0; i < schema_.arguments().size(); i++) {
     for (size_t j = i; j < schema_.arguments().size(); j++) {
       if (i == j) {
@@ -74,6 +107,16 @@ void SchemaInfo::generateAliasMaps() {
           input_alias_map_[i].insert(j);
           input_alias_map_[j].insert(i);
         }
+      }
+    }
+  }
+  for (size_t i = 0; i < schema_.arguments().size(); i++) {
+    for (size_t j = 0; j < schema_.returns().size(); j++) {
+      if (schema_.may_alias(
+              {c10::SchemaArgType::input, i},
+              {c10::SchemaArgType::output, j})) {
+        output_alias_map_[j].insert(
+            input_alias_map_[i].begin(), input_alias_map_[i].end());
       }
     }
   }
